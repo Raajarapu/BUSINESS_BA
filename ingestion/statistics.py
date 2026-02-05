08f42045754de3c6e01b594733330a5628b1466b
Suswin <laddususwin438@gmail.com>
2026-02-05 22:00:56 +0530

    Delete ingestion/statistics.py

--- ingestion/statistics.py
+++
@@ -1,6 +0,0 @@
-def descriptive_statistics(df):
-    return df.describe(include="all").to_dict()
-
-def correlation_analysis(df):
-    numeric = df.select_dtypes(include="number")
-    return numeric.corr().to_dict()
