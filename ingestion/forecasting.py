d5081da01295cef39b52a94dd4dfb1d95b502a59
Suswin <laddususwin438@gmail.com>
2026-02-05 22:01:45 +0530

    Delete ingestion/forecasting.py

--- ingestion/forecasting.py
+++
@@ -1,7 +0,0 @@
-from statsmodels.tsa.arima.model import ARIMA
-
-def forecast(series, steps=6):
-    series = series.dropna()
-    model = ARIMA(series, order=(1,1,1))
-    fitted = model.fit()
-    return fitted.forecast(steps=steps).tolist()
